6485261ce486a12a2b69e576b716c2f6c9b7d371
David McGillicuddy <dmcgill9071@gmail.com>
2015-07-31 15:34:20 +0100

    Start copying the f# version

--- src/main/scala/GameLogic/ViewCamera.scala
+++ src/main/scala/GameLogic/ViewCamera.scala
@@ -6,7 +6,20 @@ package scala.GameLogic
  * Created by David McGillicuddy on 30/07/2015.
  * This represents a floating viewpoint in the world.
  */
-class ViewCamera (origin: Vec, rho: Float, theta: Float, fov: Float, world: GameState) {
+class ViewCamera (
+    origin: Vec,
+    rho: Float,
+    theta: Float,
+    fov: Float,
+    world: GameState) {
   // TODO: copy from f# version
+  // constants
+  val RotateSpeed   = 1.0
+  val PitchSpeed    = 2.0
+  val PanSpeed      = 3.0
+  val ScrollSpeed   = 1.0
 
+  def scroll (distance: Float): Unit = {
+    return 
+  } 
 }
