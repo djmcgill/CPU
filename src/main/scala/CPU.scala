de00f23d7d3804638bbb3e707319572c765ba4cc
David McGillicuddy <dmcgill9071@gmail.com>
2015-08-05 15:47:45 +0100

    Fixed JME3 tutorial 1

--- src/main/scala/CPU.scala
+++ src/main/scala/CPU.scala
@@ -4,23 +4,21 @@ import com.jme3.math.Vector3f
 import com.jme3.scene.Geometry
 import com.jme3.scene.shape.Box
 import com.jme3.math.ColorRGBA
-import jme3test.helloworld.HelloJME3
 
 object CPU extends SimpleApplication {
 
   def main(args: Array[String]): Unit = {
-    val app = new HelloJME3()
-    app.start() // start the game
+    CPU.start()
   }
 
   @Override
   def simpleInitApp() {
-    val b = new Box(1, 1, 1) // create cube shape
-    val geom = new Geometry("Box", b)  // create cube geometry from the shape
+    val b = new Box(1, 1, 1)               // create cube shape
+    val geom = new Geometry("Box", b)      // create cube geometry from the shape
     val mat = new Material(assetManager,
-      "Common/MatDefs/Misc/Unshaded.j3md")  // create a simple material
-    mat.setColor("Color", ColorRGBA.Blue)   // set color of material to blue
-    geom.setMaterial(mat);                   // set the cube's material
-    rootNode.attachChild(geom);              // make the cube appear in the scene
+      "Common/MatDefs/Misc/Unshaded.j3md") // create a simple material
+    mat.setColor("Color", ColorRGBA.Blue)  // set color of material to blue
+    geom.setMaterial(mat);                 // set the cube's material
+    rootNode.attachChild(geom);            // make the cube appear in the scene
   }
 }
