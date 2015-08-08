9a5b6ce43097aef67a1c0fd23ebfd54993a4f3a6
David McGillicuddy <dmcgill9071@gmail.com>
2015-08-08 18:15:23 +0100

    Added a click ActionListener, now need to still get cameraOrigin-mouse direction.

--- src/main/scala/CPU.scala
+++ src/main/scala/CPU.scala
@@ -1,7 +1,10 @@
 import com.jme3.app.SimpleApplication
-import com.jme3.input.ChaseCamera
+import com.jme3.input.{MouseInput, ChaseCamera}
+import com.jme3.input.controls.{ActionListener, MouseButtonTrigger}
+import com.jme3.math.Vector3f
 import controller.Controls
 import logic.GameState
+import logic.voxels.RayCaster
 
 import rendering.GameStateRenderer
 
@@ -20,6 +23,29 @@ object CPU extends SimpleApplication {
     new ChaseCamera(cam, gs.cameraTarget, inputManager)
     rootNode.attachChild(gs.cameraTarget)
     new Controls(inputManager, gs).initKeys()
+
+    val cubeClicker = new ActionListener() {
+      def onAction(name: String, keyPressed: Boolean, tpf: Float): Unit = {
+        val rayOrigin = cam.getLocation
+
+        // TODO: cam.getDirection only looks at what the CAMERA is looking at, not useful here.
+        val rayDirection: Vector3f = ???
+
+        printf("origin: (%f, %f, %f)\n", rayOrigin.x, rayOrigin.y, rayOrigin.z)
+        printf("direction: (%f, %f, %f)\n", rayDirection.x, rayDirection.y, rayDirection.z)
+        val result = RayCaster.cast(rayOrigin, rayDirection, gs.svo)
+        result match {
+          case None => println("none")
+          case Some((hitResult, path)) =>
+            printf("path = ")
+            for (o <- path) {printf("%d", o.ix)}
+            println()
+        }
+      }
+    }
+
+    inputManager.addMapping("Click", new MouseButtonTrigger(MouseInput.BUTTON_LEFT))
+    inputManager.addListener(cubeClicker, "Click")
   }
 
 
--- src/main/scala/logic/GameState.scala
+++ src/main/scala/logic/GameState.scala
@@ -7,7 +7,9 @@ import logic.voxels.SVO
  * At the moment, the gamestate consists only of the voxels and the camera.
  */
 object GameState {
-  val initialGameState = new GameState (SVO.minimalSubdivided, new Node())
+  val initialGameState = new GameState (SVO.voxel, new Node())
 }
 
 class GameState (var svo: SVO, val cameraTarget: Node) {}
+//(17.339975, 9.966152, 0.033867)
+//direction: (-0.866999, -0.498308, -0.001693)
