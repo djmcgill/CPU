1f3c1c788c097951722e4a717438b94d0d5f6d19
David McGillicuddy <dmcgill9071@gmail.com>
2015-08-18 22:19:06 +0100

    Improve RayCasterSpec

--- src/test/scala/RayCasterSpec.scala
+++ src/test/scala/RayCasterSpec.scala
@@ -4,22 +4,14 @@ import org.scalatest._
 
 
 class RayCasterSpec extends FlatSpec with Matchers {
-
-  val Eps = 0.001
-  def almostEqual(v1: Vector3f, v2: Vector3f) = {
-    val diff = v1 subtract v2
-    def almostZero(f: Float) = math.abs(f) < Eps
-    almostZero(diff.x) && almostZero(diff.y) && almostZero(diff.z)
-  }
-
   "A single voxel" should "be hit as expected by a horizontal line" in {
     val origin = new Vector3f(0.1f, 0.1f, 5)
     val direction = new Vector3f(0, 0, -1)
     val predeterminedHit = new Vector3f(0.1f, 0.1f, 1)
     val predeterminedPath = List()
     val Some((calculatedHit, calculatedPath)) = RayCaster.cast(origin, direction, SVO.voxel)
-    almostEqual(calculatedHit, predeterminedHit) should be (true)
-    calculatedPath should be (predeterminedPath)
+    almostEqual(calculatedHit, predeterminedHit) shouldBe true
+    calculatedPath shouldBe predeterminedPath
   }
 
   "A single voxel" should "be hit as expected by a diagonal line" in {
@@ -28,8 +20,8 @@ class RayCasterSpec extends FlatSpec with Matchers {
     val predeterminedHit = new Vector3f(0.5f, 0.5f, 1)
     val predeterminedPath = List()
     val Some((calculatedHit, calculatedPath)) = RayCaster.cast(origin, direction, SVO.voxel)
-    almostEqual(calculatedHit, predeterminedHit) should be (true)
-    calculatedPath should be (predeterminedPath)
+    almostEqual(calculatedHit, predeterminedHit) shouldBe true
+    calculatedPath shouldBe predeterminedPath
   }
 
   "SVO.minimalSubdivided" should "be hit as expected by a horizontal line" in {
@@ -38,8 +30,8 @@ class RayCasterSpec extends FlatSpec with Matchers {
     val predeterminedHit = new Vector3f(0.2f, 0.2f, 0.5f)
     val predeterminedPath = List(new Octant(0))
     val Some((calculatedHit, calculatedPath)) = RayCaster.cast(origin, direction, SVO.minimalSubdivided)
-    almostEqual(calculatedHit, predeterminedHit) should be (true)
-    calculatedPath should be (predeterminedPath)
+    almostEqual(calculatedHit, predeterminedHit) shouldBe true
+    calculatedPath shouldBe predeterminedPath
   }
 
   "SVO.minimalSubdivided" should "be hit as expected by a diagonal line" in {
@@ -48,8 +40,15 @@ class RayCasterSpec extends FlatSpec with Matchers {
     val predeterminedHit = new Vector3f(0.6f, 0.6f, 1)
     val predeterminedPath = List(new Octant(7))
     val Some((calculatedHit, calculatedPath)) = RayCaster.cast(origin, direction, SVO.minimalSubdivided)
-    almostEqual(calculatedHit, predeterminedHit) should be (true)
-    calculatedPath should be (predeterminedPath)
+    almostEqual(calculatedHit, predeterminedHit) shouldBe true
+    calculatedPath shouldBe predeterminedPath
+  }
+
+  private val Eps = 0.001
+  private def almostEqual(v1: Vector3f, v2: Vector3f) = {
+    val diff = v1 subtract v2
+    def almostZero(f: Float) = math.abs(f) < Eps
+    almostZero(diff.x) && almostZero(diff.y) && almostZero(diff.z)
   }
 
 }
