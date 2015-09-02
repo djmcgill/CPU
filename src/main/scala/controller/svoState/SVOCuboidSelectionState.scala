594a773475bae3d1ca6584b6214122d3eb44f08c
David McGillicuddy <dmcgill9071@gmail.com>
2015-09-02 18:40:06 +0100

    CuboidSelectionState now draws a transparent cuboid.

--- src/main/scala/controller/svoState/SVOCuboidSelectionState.scala
+++ src/main/scala/controller/svoState/SVOCuboidSelectionState.scala
@@ -2,10 +2,14 @@ package controller.svoState
 
 import com.jme3.input.KeyInput
 import com.jme3.input.controls._
-import com.jme3.math.Vector3f
+import com.jme3.material.Material
+import com.jme3.material.RenderState.BlendMode
+import com.jme3.math.{ColorRGBA, Vector3f}
 import com.jme3.renderer.RenderManager
+import com.jme3.renderer.queue.RenderQueue.Bucket
 import com.jme3.scene.Geometry
 import com.jme3.scene.shape.Box
+import com.jme3.texture.Texture
 import controller.AbstractActionListenerState
 
 class SVOCuboidSelectionState extends AbstractActionListenerState {
@@ -13,7 +17,12 @@ class SVOCuboidSelectionState extends AbstractActionListenerState {
   private var selectedCorners: Option[(Vector3f, Vector3f)] = None
   private lazy val geometry = {
     val geo = new Geometry("Cuboid selection", new Box(1, 1, 1))
-    ??? // TODO: sort out texture and colour and transparency and stuff
+    val boxMat = new Material(app.getAssetManager, "Common/MatDefs/Misc/Unshaded.j3md")
+    geo.setMaterial(boxMat)
+
+    boxMat.setColor("Color", new ColorRGBA(1, 1, 1, 0.2f))
+    boxMat.getAdditionalRenderState.setBlendMode(BlendMode.Alpha)
+    geo.setQueueBucket(Bucket.Translucent)
     geo
   }
 
@@ -22,9 +31,13 @@ class SVOCuboidSelectionState extends AbstractActionListenerState {
   override def action(name: String, isPressed: Boolean, tpf: Float): Unit = {
     if (isPressed) {
       initialPosition = Some(pointUnderMouse())
+      app.getRootNode.attachChild(geometry)
+      app.getInputManager.setCursorVisible(false)
     } else {
-      // TODO: do something with the selected cuboid
+      // TODO: do something with the selected cuboid. Insert at all those bits?
       initialPosition = None
+      app.getRootNode.detachChild(geometry)
+      app.getInputManager.setCursorVisible(true)
     }
   }
 
@@ -35,16 +48,13 @@ class SVOCuboidSelectionState extends AbstractActionListenerState {
     val oldCorners = selectedCorners
     updateCorners(pointUnderMouse())
 
-    selectedCorners foreach {case (lowerLeftCorner, upperRightCorner) if oldCorners != selectedCorners =>
-        val newMesh = new Box(Vector3f.ZERO, upperRightCorner subtract lowerLeftCorner)
+    selectedCorners foreach {case (lowerLeftCorner, upperRightCorner) => if (oldCorners != selectedCorners) {
+        val smallVector = new Vector3f(0.1f, 0.1f, 0.1f)
+        val newMesh = new Box(Vector3f.ZERO subtract smallVector, upperRightCorner subtract lowerLeftCorner add smallVector)
         geometry.setMesh(newMesh)
         geometry.setLocalTranslation(lowerLeftCorner)
+      }
     }
-  }
-
-  // Render the geometry
-  override def render(rm: RenderManager): Unit = selectedCorners foreach {_ =>
-      rm.renderGeometry(geometry)
   }
 
   private def updateCorners(currentMousePosition: Vector3f): Unit = {
@@ -77,9 +87,9 @@ class SVOCuboidSelectionState extends AbstractActionListenerState {
     // The point at which the ray intersects the plane specified y = cameraTarget.y
     // y = origin.y + t*dir.y
     // (y - origin.y) / dir.y = t
-    assert (rayOrigin.y >= cameraTarget.y && rayDirection.y < 0)
     val Eps = 0.01f
     if (math.abs(rayDirection.y) < Eps) {rayDirection.y = -Eps}
+    assert (rayDirection.y < 0) // Weird things happen if this isn't true.
 
     val t = (cameraTarget.y - rayOrigin.y) / rayDirection.y
     val hit = rayOrigin add (rayDirection mult t)
