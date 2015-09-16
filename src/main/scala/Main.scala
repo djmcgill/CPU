d18e58ac9d1474c451ea7a2777a53d09267d6cbb
David McGillicuddy <dmcgill9071@gmail.com>
2015-09-16 19:38:33 +0100

    Added skybox textures to git

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,6 @@
 import com.jme3.app.SimpleApplication
 import com.jme3.asset.plugins.FileLocator
 import com.jme3.bullet.BulletAppState
-import com.jme3.texture.Texture
-import com.jme3.util.SkyFactory
 import controller._
 import controller.peonState._
 import controller.svoState.SVOSpatialState
