610808a1ac066d9201adba2872b34e8da9449064
David McGillicuddy <david.mcgillicuddy@moneyfarm.com>
2015-09-17 07:31:05 +0100

    Finished AStar, now to add tests.

--- src/main/scala/logic/voxels/AStar.scala
+++ src/main/scala/logic/voxels/AStar.scala
@@ -6,17 +6,47 @@ import com.jme3.math.Vector3f
 import scala.collection.mutable
 
 object AStar {
-  def apply(start: Vector3f, goal: Vector3f, navGrid: SVONavGrid): List[Vector3f] = {
-    // TODO: also include path smoothing when possible
-    // TODO: also need to work out the actual path?
-    def pairWithDistance(v: Vector3f) = (v.distanceSquared(goal), v)
-    val openSet = mutable.PriorityQueue[(Float, Vector3f)](pairWithDistance(start))(Ordering.by(_._1))
-    val closedSet = mutable.Set[Vector3f]()
-    val cameFrom = mutable.Map[Vector3f, Vector3f]()
+  def apply(
+    start: Vector3f,
+    goal: Vector3f,
+    maxIterations: Int,
+    closeEnough: Float,
+    navGrid: SVONavGrid)
+  : Option[List[Vector3f]] = {
+    def shortest[A]  = Ordering.by[(Float, A), Float](_._1).reverse
+    val openSet      = mutable.PriorityQueue[(Float, Vector3f)] ((start.distance(goal), start))(shortest)
+    val closedSet    = mutable.Set[Vector3f]()
+    val cameFrom     = mutable.Map[Vector3f, Vector3f]()
+    val pathLengthTo = mutable.Map[Vector3f, Float]((start, 0))
 
-    def canGoDirectlyTo(from: Vector3f, to: Vector3f, bulletAppState: BulletAppState): Boolean = ???
+    for (_ <- 0 until maxIterations) {
+      if (openSet.isEmpty) {return None}
+      val current = openSet.dequeue()._2
+      if (current.distance(goal) <= closeEnough) {
+        return Some(reconstructPath(cameFrom)(current))
+      }
+      closedSet.add(current)
 
+      navGrid.validNeighbours(current).filter(!closedSet.contains(_)).foreach{ neighbour =>
+        val pathLengthToNeighbour = pathLengthTo.get(current).get + current.distance(neighbour)
+        val isFurtherThanAlreadyKnown = pathLengthTo.get(neighbour) exists (pathLengthToNeighbour > _)
+        if (!isFurtherThanAlreadyKnown) {
+          cameFrom     += ((neighbour, current))
+          pathLengthTo += ((current, pathLengthToNeighbour))
+          // WARNING: Nodes will appear in the openSet multiple times.
+          openSet      += ((pathLengthToNeighbour + neighbour.distance(goal), neighbour))
+        }
+      }
+    }
+    None
+  }
 
-    ???
+  def reconstructPath(cameFrom: mutable.Map[Vector3f, Vector3f]): Vector3f => List[Vector3f] = {
+    // TODO: use trampolining here
+    def reconstructPathGo(pathSoFar: List[Vector3f])(current: Vector3f): List[Vector3f] = {
+      val previous = cameFrom.get(current).get
+      reconstructPathGo(current :: pathSoFar)(previous)
+    }
+    current => reconstructPathGo(Nil)(current).reverse
   }
 }
